3e12473d6b7eb42cdcaedee76d6174e324368410
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2025-01-08 23:47:41 -0500

    Add files via upload

---
+++ Govee/v2/Mavrrick.Goveev2H6093StarlightDriver.groovy
@@ -0,0 +1,194 @@
+// Hubitat driver for Starlight Projector
+// Version 2.1.0
+//
+// 05/07/2024 2.1.0 update to support Nested devices under Parent devices
+
+
+import hubitat.helper.InterfaceUtils
+import hubitat.helper.HexUtils
+import groovy.transform.Field
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import groovy.json.JsonBuilder
+
+#include Mavrrick.Govee_Cloud_API
+#include Mavrrick.Govee_Cloud_RGB
+#include Mavrrick.Govee_Cloud_Level
+#include Mavrrick.Govee_LAN_API
+
+def commandPort() { "4003" }
+
+metadata {
+	definition(name: "Govee v2 H6093 Starlight Driver", namespace: "Mavrrick", author: "Mavrrick") {
+		capability "Switch"
+		capability "Light"
+		capability "SwitchLevel"
+		capability "Refresh"
+        capability "Initialize"
+        capability "LightEffects"
+        
+        attribute "cloudAPI", "string"
+        attribute "online", "string"        
+        
+        command "activateDIY", [
+            [name: "diyName", type: "STRING", description: "DIY Number to activate"]
+           ]
+    }
+
+	preferences {		
+		section("Device Info") {
+            input("pollRate", "number", title: "Polling Rate (seconds)\nDefault:300", defaultValue:300, submitOnChange: true, width:4)
+			input(name: "aRngBright", type: "bool", title: "Alternate Brightness Range", description: "For devices that expect a brightness range of 0-254", defaultValue: false)
+            input(name: "lanControl", type: "bool", title: "Enable Local LAN control", description: "This is a advanced feature that only worked with some devices. Do not enable unless you are sure your device supports it", defaultValue: false)
+            if (lanControl) {
+            input("ip", "text", title: "IP Address", description: "IP address of your Govee light", required: false)
+            input(name: "lanScenes", type: "bool", title: "Enable Local LAN Scene Control", description: "If this is active your device will use Local Scenes control. Leave off to use Scenes/DIY's/Snapshots from the cloud API", defaultValue: false)         
+            input("fadeInc", "decimal", title: "% Change each Increment of fade", defaultValue: 1)
+            }
+            input(name: "debugLog", type: "bool", title: "Debug Logging", defaultValue: false)
+            input("descLog", "bool", title: "Enable descriptionText logging", required: true, defaultValue: true) 
+		}
+		
+	}
+}
+
+///////////////////////////////////////////////
+// Methods to setup manage device properties //
+///////////////////////////////////////////////
+
+def poll() {
+    if (debugLog) {log.warn "poll(): Poll Initated"}
+	refresh()
+}
+
+def refresh() {
+    if (debugLog) {log.warn "refresh(): Performing refresh"}
+        unschedule(poll)
+        if (pollRate > 0) runIn(pollRate,poll)
+        getDeviceState()
+}
+
+def updated() {
+    initialize()
+    sceneLoad()
+}
+
+def initialize(){
+    if (debugLog) {log.warn "initialize(): Driver Initializing"}    
+    if (device.currentValue("cloudAPI") == "Retry") {
+        if (debugLog) {log.error "initialize(): Cloud API in retry state. Reseting "}
+        sendEvent(name: "cloudAPI", value: "Initialized")
+        }
+    if(device.getDataValue("commands").contains("lightScene")) {
+        sendEvent(name: "effectNum", value: 0) }
+    unschedule()
+    if (pollRate > 0) {
+        pollRateInt = pollRate.toInteger()
+        randomOffset(pollRateInt)
+        runIn(offset,poll)
+    }
+    if (debugLog) runIn(1800, logsOff)
+    
+}
+
+
+def installed(){
+    if (debugLog) {log.warn "installed(): Driver Installed"}
+    if(device.getDataValue("commands").contains("color")) {
+        sendEvent(name: "hue", value: 0)
+        sendEvent(name: "saturation", value: 100)
+    }
+    if(device.getDataValue("commands").contains("lightScene")) {
+        sendEvent(name: "effectNum", value: 0) }
+    if (pollRate > 0) runIn(pollRate,poll)
+    retrieveScenes2()
+    retrieveStateData()
+    getDevType()
+}
+
+def logsOff() {
+    log.warn "debug logging disabled..."
+    device.updateSetting("debugLog", [value: "false", type: "bool"])
+}
+
+def sceneLoad() {
+    
+    if (lanScenes == null) {
+    if (debugLog) {log.debug "sceneLoad(): lanScenes not set"}
+        device.updateSetting('lanScenes', [type: "bool", value: true])
+    }
+    
+    if (lanControl && lanScenes) { 
+        getDevType()
+        retrieveScenes()   
+    } else if ((lanControl == false) || (lanControl && lanScenes == false)) { 
+        retrieveScenes2()
+        retrieveStateData()
+        if (state.diyScene.isEmpty()) {
+            if (debugLog) {log.warn "configure(): retrieveScenes2() returned empty diyScenes list. Running retrieveDIYScenes() to get list from API"}
+            retrieveDIYScenes()
+        }
+    }
+}
+
+
+/////////////////////////
+// Commands for Driver //
+/////////////////////////
+
+def on() {
+    if (lanControl) {
+        lanOn() }
+    else {
+        cloudOn()
+        }
+}
+
+def off() {
+    if (lanControl) {
+        lanOff() }
+    else {
+        cloudOff()
+        }
+}
+
+def setLevel(float v,duration = 0) {
+    if (lanControl) {
+        lanSetLevel(v,duration) 
+    } else {
+        cloudSetLevel( v, 0)
+        }
+}
+
+def  setEffect(effectNo) {
+    if (lanControl) {
+        lanSetEffect (effectNo)
+    } else { 
+        cloudSetEffect (effectNo)
+    }
+}
+
+def setNextEffect() {
+    if (lanControl) {
+        lanSetNextEffect ()
+    } else {
+        cloudSetNextEffect ()
+    }
+} 
+      
+def setPreviousEffect() {
+    if (lanControl) {
+        lanSetPreviousEffect ()
+    } else {
+        cloudSetPreviousEffect ()         
+    }
+}
+
+
+def activateDIY(diyActivate) {
+    if (lanControl) {
+        lanActivateDIY (diyActivate)
+    } else {
+        cloudActivateDIY (diyActivate)
+    }
+}
