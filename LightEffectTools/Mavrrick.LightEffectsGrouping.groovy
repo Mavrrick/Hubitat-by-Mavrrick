0fe4816c7a7be30b031faf2888ed4f362e97e558
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2025-11-13 12:17:51 -0500

    Update Mavrrick.LightEffectsGrouping.groovy

--- LightEffectTools/Mavrrick.LightEffectsGrouping.groovy
+++ LightEffectTools/Mavrrick.LightEffectsGrouping.groovy
@@ -285,7 +285,8 @@ private def appButtonHandler(button) {
         endAction()
     } else if (button == "createDevice") {
     	childCount = getChildDevices().size()
-        if (childCount > '0') {
+        if(debugEnable) log.debug "Child count is ${childCount}."
+        if (childCount == 0) {
             addChildDevice('Mavrrick', 'LightEffect Group', app.getLabel()+'_Device' , location.hubs[0].id, [
                 'name': 'LightEffect Group Device ', 
                 'label': app.getLabel()+'_Device',
