8568be3cb55f53f9b5f28389485c28dff6940e81
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2025-12-29 14:24:39 -0500

    Add API Reset call when Device initializes

--- Govee/v2/Mavrrick.Goveev2ColorLights3Driver.groovy
+++ Govee/v2/Mavrrick.Goveev2ColorLights3Driver.groovy
@@ -121,6 +121,7 @@ def initialize(){
         }
     initDefaultValues()
     unschedule()
+    if (lanControl) resetApiStatus()
     retrieveIPAdd()
     if (pollRate > 0) {
         pollRateInt = pollRate.toInteger()
