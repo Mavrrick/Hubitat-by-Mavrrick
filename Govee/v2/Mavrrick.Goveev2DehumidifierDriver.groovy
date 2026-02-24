b7872f9234f9567b17e0f088daf5f1106a4a9720
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2026-02-24 10:06:09 -0500

    Add files via upload

---
+++ Govee/v2/Mavrrick.Goveev2DehumidifierDriver.groovy
@@ -0,0 +1,213 @@
+// Hubitat driver for Govee Appliances using Cloud API
+// Version 2.1.0
+//
+// 05/07/2024 2.1.0 update to support Nested devices under Parent devices
+
+
+// Includes of library objects
+#include Mavrrick.Govee_Cloud_API
+#include Mavrrick.Govee_Cloud_Life
+#include Mavrrick.Govee_Cloud_MQTT
+
+import groovy.json.JsonSlurper 
+import java.util.Random 
+
+metadata {
+	definition(name: "Govee v2 Dehumidifier Driver", namespace: "Mavrrick", author: "Mavrrick") {
+		capability "Switch"
+		capability "Actuator"
+        capability "Initialize"
+        capability "Refresh"
+        capability "Configuration"        
+        
+		attribute "desiredHumidity", "number"
+        attribute "gear", "number"
+        attribute "mode", "number"
+        attribute "modeValue", "number"
+        attribute "modeDescription", "string"
+        attribute "pollInterval", "number"        
+        attribute "cloudAPI", "string"
+        attribute "online", "string"        
+        attribute "waterFullEvent", "string"
+        
+        command "workingMode", [[name: "mode", type: "ENUM", constraints: [ 'Manual',      'Custom',       'Auto'], description: "Mode of device"],
+                          [name: "gearMode", type: "NUMBER", description: "When Mode is Manual sets hudifier speed. When set to Auto sets desired Humidity"]]
+        command "desiredHumidity", [[name: desiredHumidityValue, type: 'NUMBER', description: "Set the desired Humidity the device will try to maintain", range: 40-80]]
+        command "changeInterval", [[name: "changeInterval", type: "NUMBER",  description: "Change Polling interval range from 0-600", range: 0-600, required: true]]
+        
+    }
+
+	preferences {		
+		section("Device Info") {
+            input("pollRate", "number", title: "Polling Rate (seconds)\nDefault:300", defaultValue:300, submitOnChange: true, width:4) 
+            input(name: "debugLog", type: "bool", title: "Debug Logging", defaultValue: false)
+            input("descLog", "bool", title: "Enable descriptionText logging", required: true, defaultValue: true) 
+		}
+		
+	}
+}
+
+//////////////////////////////////////
+// Standard Methods for all drivers //
+//////////////////////////////////////
+
+// reset of device settings when preferences updated.
+def updated() {
+    if (debugLog) {log.info "updated(): device updated "}
+    unschedule()
+    if (debugLog) runIn(1800, logsOff)
+    retrieveStateData()
+    if (getChildDevices().size() == 0) {
+        if (device.getDataValue("commands").contains("nightlightToggle")) {
+        runIn(10, addLightDeviceHelper)
+        }
+    }
+
+}
+
+// linital setup when device is installed.
+def installed(){
+    retrieveStateData()
+    if (getChildDevices().size() == 0) {
+        if (device.getDataValue("commands").contains("nightlightToggle")) {
+        runIn(10, addLightDeviceHelper)
+        }
+    }
+    poll()
+
+}
+
+// initialize devices upon install and reboot.
+def initialize() {
+     if (device.currentValue("cloudAPI") == "Retry") {
+        if (debugLog) {log.error "initialize(): Cloud API in retry state. Reseting "}
+        sendEvent(name: "cloudAPI", value: "Initialized")
+    }
+    unschedule()
+    if (debugLog) runIn(1800, logsOff)
+    retrieveStateData()
+    if (getChildDevices().size() == 0) {
+        if (device.getDataValue("commands").contains("nightlightToggle")) {
+        addLightDeviceHelper()
+        }
+    }
+    if (pollRate > 0) {
+        pollRateInt = pollRate.toInteger()
+        randomOffset(pollRateInt)
+        runIn(offset,poll)
+    }
+
+}
+
+// update data for the device
+def refresh() {
+    if (debugLog) {log.info "refresh(): Performing refresh"}
+    unschedule(poll)
+    poll()
+    if (device.currentValue("connectionState") == "connected") {
+    }
+}
+
+// retrieve setup values and initialize polling and logging
+def configure() {
+    if (debugLog) {log.info "configure(): Driver Updated"}
+    unschedule()
+    if (pollRate > 0) runIn(pollRate,poll)     
+    retrieveStateData()    
+    if (debugLog) runIn(1800, logsOff)
+
+}
+
+////////////////////
+// Helper methods //
+////////////////////
+
+logsOff  // turn off logging for the device
+def logsOff() {
+    log.info "debug logging disabled..."
+    device.updateSetting("debugLog", [value: "false", type: "bool"])
+}
+
+poll // retrieve device status
+def poll() {
+    if (debugLog) {log.info "poll(): Poll Initated"}
+	getDeviceState()
+    if (pollRate > 0) runIn(pollRate,poll)
+}	
+
+//////////////////////
+// Driver Commands // 
+/////////////////////
+
+def on() {
+        cloudOn()
+}
+
+def off() {
+        cloudOff()
+}
+
+def desiredHumidity(value) {
+    sendCommand("humidity", value, "devices.capabilities.range")        
+}
+
+def workingMode(mode, gear=0){
+    log.debug "workingMode(): Processing Working Mode command. ${mode} ${gear}"
+    sendEvent(name: "cloudAPI", value: "Pending")
+    switch(mode){
+        case "Manual":
+            modenum = 1;        
+        break;
+        case "Custom":
+            modenum = 2;
+            gear = 0;
+        break;
+        case "Auto":
+            modenum = 3;
+        break;
+    default:
+    log.debug "not valid value for mode";
+    break;
+    }
+    values = '{"workMode":'+modenum+',"modeValue":'+gear+'}'
+    sendCommand("workMode", values, "devices.capabilities.work_mode")
+}  
+
+///////////////////////////////////////////////////
+// Heler routine to create child devices         //
+///////////////////////////////////////////////////
+
+def addLightDeviceHelper() {
+	//Driver Settings
+    driver = "Govee v2 Life Child Light Device"
+    deviceID = device.getDataValue("deviceID")
+    deviceName = device.label+"_Nightlight"
+    deviceModel = device.getDataValue("deviceModel")
+	Map deviceType = [namespace:"Mavrrick", typeName: driver]
+	Map deviceTypeBak = [:]
+	String devModel = deviceModel
+	String dni = "Govee_${deviceID}_Nightlight"
+    APIKey = device.getDataValue("apiKey")
+	Map properties = [name: driver, label: deviceName, deviceID: deviceID, deviceModel: deviceModel, apiKey: APIKey]
+//    log.debug "Setup detail '${properties}' driver failed"
+    if (debugLog) { log.debug "Creating Child Device"}
+
+	def childDev
+	try {
+		childDev = addChildDevice(deviceType.namespace, deviceType.typeName, dni, properties)
+	}
+	catch (e) {
+		log.warn "The '${deviceType}' driver failed"
+		if (deviceTypeBak) {
+			logWarn "Defaulting to '${deviceTypeBak}' instead"
+			childDev = addChildDevice(deviceTypeBak.namespace, deviceTypeBak.typeName, dni, properties)
+		}
+	} 
+}
+
+def retNightlightScene(){
+    scenes = state.nightlightScene 
+    if (debugLog) { log.debug "retNightlightScene(): Nightlight Scenes are  " + scenes }
+    return scenes
+}
+
