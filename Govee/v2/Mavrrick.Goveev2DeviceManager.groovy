7a96f8523739438eb5a1ef5bc07e324481f10e74
Mavrrick <36817650+Mavrrick@users.noreply.github.com>
2026-08-26 16:20:08 -0400

    Update Mavrrick.Goveev2DeviceManager.groovy

--- Govee/v2/Mavrrick.Goveev2DeviceManager.groovy
+++ Govee/v2/Mavrrick.Goveev2DeviceManager.groovy
@@ -196,9 +196,11 @@ def disconnected() {
 
 void parse(String event) {
     
-//    if (collectStats) long startTime = now()
-    if (collectStats ?: false) long startTime = now()
+    long startTime = 0L
 
+    if (collectStats ?: false) {
+        startTime = now()
+    }
     if (debugLog) log.info "parse() Parse call started at ${startTime}"
     if (event.contains("topic")) {
         if (debugLog) log.info "parse() MQTT message recieved. Parsing and sending to device"
